e181cfc85491eb68e93667c7816142f78dc8ab7b
Tsigab <107885237+Tsigab1996@users.noreply.github.com>
2023-02-05 22:04:54 -0600

    First commit

---
+++ WAA-PMP/src/main/java/edu/miu/waapmp/WaaPmpApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.waapmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaPmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaaPmpApplication.class, args);
+    }
+
+}
---
+++ WAA-PMP/src/test/java/edu/miu/waapmp/WaaPmpApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.waapmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaPmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
